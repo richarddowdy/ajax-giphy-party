9d68c51727177f05ccc96f33f1ac57c45b5bdbc6
Richard Dowdy <richard.dowdy0@gmail.com>
2020-01-14 16:09:12 -0800

    api call working

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
+
+$(".searchForm").on("submit", async function(e){
+  e.preventDefault();
+  
+  let searchItem = $("#searchItem").val();
+
+  let response = await axios.get("http://api.giphy.com/v1/gifs/search", { params: {q:searchItem, api_key:"ZFwUmtWDV46D4e8VPwzCHQlkGRCaHJ5a"}});
+  console.log(response.data);
+
+  randomGif(response.data);
+
+  
+})
+
+function randomGif(input){
+  let randomInt = Math.floor(Math.random()*input.length);
+  let randGif = reponse.data[randomInt];
+  return randGif;
+}
+
+
+
+
 console.log("Let's get this party started!");
