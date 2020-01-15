2115bd38788afafd7a03631a64a326bdefe05c6b
sairina <sairina.merinotsui@gmail.com>
2020-01-14 17:04:54 -0800

    finished app.js

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
+$(function(){
 
-$(".searchForm").on("submit", async function(e){
-  e.preventDefault();
+  $("#searchForm").on("submit", async function(e){
+    e.preventDefault();
+    
+    let searchItem = $("#searchItem").val();
   
-  let searchItem = $("#searchItem").val();
-
-  let response = await axios.get("http://api.giphy.com/v1/gifs/search", { params: {q:searchItem, api_key:"ZFwUmtWDV46D4e8VPwzCHQlkGRCaHJ5a"}});
-  console.log(response.data);
-
-  randomGif(response.data);
-
+    let response = await axios.get("http://api.giphy.com/v1/gifs/search", { params: 
+      {
+        q: searchItem, 
+        api_key: "ZFwUmtWDV46D4e8VPwzCHQlkGRCaHJ5a"
+      }
+    });  
+    randomGif(response.data);
+  });
   
+  function randomGif(input){
+    let randomInt = Math.floor(Math.random()* input.data.length);
+    let randURL = input.data[randomInt].images.downsized_large.url;
+    
+    let $soloGif = $("<img>");
+    $("container").append($soloGif.attr("src", randURL));
+    return randURL;
+  }
+
+  $("#removeBtn").on("click", function(){
+    $("container").empty();
+  });
+  
+  console.log("Let's get this party started!");
 })
 
-function randomGif(input){
-  let randomInt = Math.floor(Math.random()*input.length);
-  let randGif = reponse.data[randomInt];
-  return randGif;
-}
-
-
-
-
-console.log("Let's get this party started!");
+// let $soloGif = $("<img>", { src: randGif }); //another way to create element and set attr
